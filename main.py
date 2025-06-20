eaf9723f1bc7e256835481da260ed55512787c52
DallogFheir <dallog.fheir@gmail.com>
2025-06-21 01:03:07 +0200

    feat: debug mode

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from src.run import run
 
-DEBUG = True
+DO_MAKE_IT_QUICKER = False
+DO_USE_MOCK_HEADSET = True
+
+BRAINACCESS_CAP_NAME = "BA MAXI 011"
 
 
 if __name__ == "__main__":
-    run(debug=DEBUG)
+    run(
+        brainaccess_cap_name=BRAINACCESS_CAP_NAME,
+        do_make_it_quicker=DO_MAKE_IT_QUICKER,
+        do_use_mock_headset=DO_USE_MOCK_HEADSET,
+    )
--- src/run.py
+++ src/run.py
@@ -14,24 +14,46 @@ DEBUG_SENTENCES_IN_BLOCK_COUNT = 3
 DEBUG_RELAX_SCREEN_TIMEOUT_MILLIS = 5 * 1000
 
 
-def run(*, debug: bool) -> None:
+def run(
+    *,
+    brainaccess_cap_name: str,
+    do_make_it_quicker: bool = False,
+    do_use_mock_headset: bool = False,
+) -> None:
+    if do_use_mock_headset:
+        headset = MockEEGHeadset()
+    else:
+        from data_acquisition.eeg_headset.brainaccess import BrainAccessV3Headset
+        from data_acquisition.eeg_headset.brainaccess.devices import (
+            BRAINACCESS_MAXI_32_CHANNEL,
+        )
+
+        headset = BrainAccessV3Headset(
+            device_name=brainaccess_cap_name,
+            device_channels=BRAINACCESS_MAXI_32_CHANNEL,
+        )
+
     participant_id = datetime.now().strftime(
         "%Y%m%d_%H%M%S"
     )  # TODO: zamienic na ID z ankiety
 
     display_mode = (
-        WindowedDisplayMode(width=800, height=600) if debug else FullscreenDisplayMode()
+        WindowedDisplayMode(width=800, height=600)
+        if do_make_it_quicker
+        else FullscreenDisplayMode()
     )
     gui = PygameGui(display_mode=display_mode, window_title="NeuroGuard")
 
-    headset = MockEEGHeadset()
-
     config = Config(
         sentence_count=(
-            DEBUG_SENTENCES_IN_BLOCK_COUNT if debug else SENTENCES_IN_BLOCK_COUNT
+            DEBUG_SENTENCES_IN_BLOCK_COUNT
+            if do_make_it_quicker
+            else SENTENCES_IN_BLOCK_COUNT
         ),
         relax_screen_timeout_millis=(
-            DEBUG_RELAX_SCREEN_TIMEOUT_MILLIS if debug else RELAX_SCREEN_TIMEOUT_MILLIS
+            DEBUG_RELAX_SCREEN_TIMEOUT_MILLIS
+            if do_make_it_quicker
+            else RELAX_SCREEN_TIMEOUT_MILLIS
         ),
     )
 
